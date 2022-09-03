753b22b2a1c4213ff8a46024a824fa46141884e8
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-03 16:46:39 +0600

    2 - Creating Models

--- My_First_Project/first_app/models.py
+++ My_First_Project/first_app/models.py
@@ -10,10 +10,26 @@ CREATE TABLE Person (
 )
 
 This is equvalent to under this code 
-'''
+
 
 
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+'''
+
+
+class Musician(models.Model):
+    # id = models.AutoField(primary_key=True) # automatically declare if there no primary key
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    # CASCADE: if any Musician delete, then here his Album automatcally delete
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
