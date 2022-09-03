26c9976531b056eb7e774e62f65858fff91e7477
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-03 19:34:49 +0600

    5.4 - Creating Django Form 3: widget

--- My_First_Project/first_app/forms.py
+++ My_First_Project/first_app/forms.py
@@ -4,8 +4,19 @@ from django import forms
 class user_form(forms.Form):
 
     # <label for = "user_name" > Full Name: < /label >
-    # <input type = "text" name = "user_name" value = "Asad" required >
-    user_name = forms.CharField(required=False, label="Full Name", initial="Asad")
+    # <input type = "text" name = "user_name" placeholder="Enter your full name"  value = "" style="width:300px" required >
+    # set style:
+    user_name = forms.CharField(required=False, widget=forms.TextInput(
+        attrs={
+            'placeholdder': 'Enter your full name',
+            'style': 'width: 300px'
+        }))
+
+    user_dob = forms.DateField(label="Date of Birth", widget=forms.TextInput(
+        attrs={
+            'type': 'date'
+        }
+    ))
 
     # <label for = "user_email" > User Email: < /label >
     #  <input type = "email" name = "user_email" value = "" required >
