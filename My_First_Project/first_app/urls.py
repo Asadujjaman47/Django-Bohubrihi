530ec614c6df3d0eba198b2fa61d54c6603fc903
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-03 17:49:46 +0600

    5.1- Form with HTML

--- My_First_Project/first_app/urls.py
+++ My_First_Project/first_app/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    path('', views.index, name='index')
-
+    path('', views.index, name='index'),
+    path('form/', views.form, name='form')
 ]
--- My_First_Project/first_app/views.py
+++ My_First_Project/first_app/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponse
 from first_app.models import Musician, Album
 
 # Create your views here.
-
-
 def index(request):
     # SELECT * FROM Musician ORDER BY first_name
     musician_list = Musician.objects.order_by('first_name')
@@ -13,3 +11,8 @@ def index(request):
         'musician': musician_list
     }
     return render(request, 'first_app/index.html', context=diction)
+
+
+def form(request):
+    diction = {}
+    return render(request, 'first_app/form.html', context=diction)
