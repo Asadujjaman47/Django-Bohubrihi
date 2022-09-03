52745877a3db79cb931f5a2413b5c722b00b965b
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-03 18:30:34 +0600

    5.3 - Creating a Django Form 2

--- My_First_Project/first_app/forms.py
+++ My_First_Project/first_app/forms.py
@@ -5,8 +5,8 @@ class user_form(forms.Form):
 
     # <label for = "user_name" > Full Name: < /label >
     # <input type = "text" name = "user_name" value = "Asad" required >
-    user_name = forms.CharField()
+    user_name = forms.CharField(required=False, label="Full Name", initial="Asad")
 
     # <label for = "user_email" > User Email: < /label >
     #  <input type = "email" name = "user_email" value = "" required >
-    user_email = forms.EmailField()
+    user_email = forms.EmailField(label="User Email")
