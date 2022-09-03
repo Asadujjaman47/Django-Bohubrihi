97381b6becd44d30614b4a60b6f20293067fd10a
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-03 16:56:23 +0600

    3 - Model Objects

--- My_First_Project/first_app/models.py
+++ My_First_Project/first_app/models.py
@@ -26,6 +26,10 @@ class Musician(models.Model):
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
 
+    # this function is for showing data in Object
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
