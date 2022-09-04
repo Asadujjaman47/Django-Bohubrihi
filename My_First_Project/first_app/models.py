b4a360ad8108fd851604477bc6be52751aa61d1c
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-04 14:28:02 +0600

    8.1 - Connecting MySQL Database

--- My_First_Project/first_app/models.py
+++ My_First_Project/first_app/models.py
@@ -2,23 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-'''
-CREATE TABLE Person (
-    "id" serial NOT NULL PRIMARY KEY,
-    "first_name" varchar(30) NOT NULL,
-    "last_name" varchar(30) NOT NULL
-)
-
-This is equvalent to under this code 
-
-
-
-class Person(models.Model):
-    id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-'''
-
 
 class Musician(models.Model):
     # id = models.AutoField(primary_key=True) # automatically declare if there no primary key
