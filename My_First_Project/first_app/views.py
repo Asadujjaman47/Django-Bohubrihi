b5225cc76afd37c0bb13b9f73f59cb961e57ee6c
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-04 22:56:53 +0600

    8.8 - Update Data - Part 3

--- My_First_Project/first_app/views.py
+++ My_First_Project/first_app/views.py
@@ -57,3 +57,34 @@ def album_form(request):
 
     diction = {'title': "Add Album", 'album_form': form}
     return render(request, 'first_app/album_form.html', context=diction)
+
+
+def edit_artist(request, artist_id):
+    artist_info = Musician.objects.get(pk=artist_id)
+    form = forms.MusicianForm(instance=artist_info)
+
+    if request.method == 'POST':
+        form = forms.MusicianForm(request.POST, instance=artist_info)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return album_list(request, artist_id)
+
+    diction = {'edit_form': form}
+    return render(request, 'first_app/edit_artist.html', context=diction)
+
+
+def edit_album(request, album_id):
+    album_info = Album.objects.get(pk=album_id)
+    form = forms.AlbumForm(instance=album_info)
+    diction = {}
+
+    if request.method == 'POST':
+        form = forms.AlbumForm(request.POST, instance=album_info)
+
+        if form.is_valid():
+            form.save(commit=True)
+            diction.update({'success_text': 'Successfully Updated!'})
+
+    diction.update({'edit_form': form})
+    return render(request, 'first_app/edit_album.html', context=diction)
