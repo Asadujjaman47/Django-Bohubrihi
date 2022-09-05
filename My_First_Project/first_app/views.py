b77286f978a8fdf6748b3e18c81cbce75cca43bb
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-05 11:55:54 +0600

    10.2 - List View

--- My_First_Project/first_app/views.py
+++ My_First_Project/first_app/views.py
@@ -1,19 +1,24 @@
 from multiprocessing import context
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView
+from first_app import models
 
 # Create your views here.
-
 # functionbase views:
 # def index(request):
 #     return render(request, 'template', context={})
 
 
 # classbase view:
-class IndexView(TemplateView):
-    template_name = 'first_app/index.html'
+# class IndexView(TemplateView):
+#     template_name = 'first_app/index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['sample_text_1'] = 'SamPle Text 1'
-        context['sample_text_2'] = 'SamPle Text 2'
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['sample_text_1'] = 'SamPle Text 1'
+#         context['sample_text_2'] = 'SamPle Text 2'
+#         return context
+
+class IndexView(ListView):
+    context_object_name = 'musician_list'
+    model = models.Musician
+    template_name = 'first_app/index.html'
